609eb4944c48ecf8819f72b2836447ddb47cfce8
Ana Bujan <ana@eisberg-labs.com>
2025-05-11 20:09:50 +0200

    configure admob

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,45 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {Slot, Stack} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {requestTrackingPermissionsAsync} from 'expo-tracking-transparency';
+import React, {useEffect} from 'react';
+import {AdEventType, AdsConsent, AdsConsentStatus, InterstitialAd, TestIds} from 'react-native-google-mobile-ads';
+import mobileAds from 'react-native-google-mobile-ads/src';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [adsLoaded, setAdsLoaded] = React.useState(false);
+  useEffect(() => {
+    const prepare = async () => {
+      // TODO: if the ATT doesn't show up, add a small delay
+      await requestTrackingPermissionsAsync()
+      try {
+        const consentInfo = await AdsConsent.requestInfoUpdate();
+        if (consentInfo.isConsentFormAvailable && consentInfo.status === AdsConsentStatus.REQUIRED) {
+          await AdsConsent.showForm();
+        }
+        await mobileAds().initialize();
+        setAdsLoaded(true);
+      } catch (e) {
+        console.log('error', e);
+      }
+    }
+    void prepare();
+  }, []);
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+
+  if (!adsLoaded) {
     return null;
   }
 
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
